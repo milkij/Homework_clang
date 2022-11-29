b2ebfb57fc43a5fef7ebb28072d432d90fe3c220
shirokov.mos@ya.ru <shirokov.mos@ya.ru>
2022-11-29 17:37:16 +0300

    Finish task1 from module 23.

--- module23/task1.cpp
+++ module23/task1.cpp
@@ -1,9 +1,28 @@
 //
-// Created by Александр Широков on 02.11.2022.
+// Created by Александр Широков on 29.11.2022.
 //
 #include <iostream>
 
+#define MONDAY 1
+#define THUESDAY 2
+#define WEDNESDAY 3
+#define THURSDAY 4
+#define FRIDAY 5
+#define SATURDAY 6
+#define SUNDAY 7
+
 int main() {
-    std::cerr << "Hello, World!" << std::endl;
+    std::cout << "Input the day of week by number and you can see the name of the day" << std::endl;
+    std::cout << "Input num of day of week: ";
+    int day;
+    std::cin >> day;
+    if(MONDAY==day) std::cout<<"Monday";
+    else if(THUESDAY==day) std::cout<<"Thusday";
+    else if(WEDNESDAY==day) std::cout<<"Wednesday";
+    else if(THURSDAY==day) std::cout<<"Thursday";
+    else if(FRIDAY==day) std::cout<<"Friday";
+    else if(SATURDAY==day) std::cout<<"Saturday";
+    else if(SUNDAY==day) std::cout<<"Sunday";
+    else std::cout << "There is no such day";
     return 0;
 }
