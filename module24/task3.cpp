bba7e086a7fbc26f85131154f2a15c2d5a6c705f
shirokov.mos@ya.ru <shirokov.mos@ya.ru>
2022-12-28 10:58:43 +0300

    module 24

--- module24/task3.cpp
+++ module24/task3.cpp
@@ -11,6 +11,7 @@ void delay(int ms)
 int main(){
 #if 1
     int min,sec;
+    printf("Input min and sec: ");
     std::cin>>min>>sec;
     std::time_t finish_time = std::time(nullptr)+(std::time_t)(min*60+sec)-1;
     while(true){
